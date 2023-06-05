5772d847fb7b20d28cb53cbc9fae93e6719c4e9d
sebaherrera <sebaherrera152@gmail.com>
2023-06-05 14:58:53 -0300

    Agrego ejemplos de Fourier con numpy y con Scipy

--- fourier/fouriercNumpy.py
+++ fourier/fouriercNumpy.py
@@ -8,14 +8,14 @@ sr = 2000
 ts = 1.0/sr
 t = np.arange(0,1,ts)
 
-freq = 10
+freq = 1
 x = 3*np.sin(2*np.pi*freq*t)
 
-#freq = 4
-#x += np.sin(2*np.pi*freq*t)
+freq = 4
+x += np.sin(2*np.pi*freq*t)
 
-#freq = 7   
-#x += 0.5* np.sin(2*np.pi*freq*t)
+freq = 7   
+x += 0.5* np.sin(2*np.pi*freq*t)
 
 plt.figure(figsize = (8, 6))
 plt.plot(t, x, 'r')
@@ -41,7 +41,7 @@ plt.subplot(121)
 plt.stem(freq, np.abs(X), 'b',markerfmt=" ", basefmt="-b")
 plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('FFT Amplitud |X(freq)|')
-plt.xlim(0, 100)
+plt.xlim(-10, 10)
 plt.title(" Respuesta en frecuencia de Funcion Senoidal")
 
 plt.subplot(122)
