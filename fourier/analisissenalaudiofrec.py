2f8ce5f774342a9428fcd23f525de1e48de35606
sebaherrera <sebaherrera152@gmail.com>
2023-06-27 16:41:46 -0300

    Aplicacion de un filtro pasa bajo y graficas del espectro a 1000 Hz

--- fourier/analisissenalaudiofrec.py
+++ fourier/analisissenalaudiofrec.py
@@ -44,6 +44,7 @@ senial_fft_mod = np.abs(senial_fft) / N     # Respetando la relación de Parceva
 fig1, ax1 = plt.subplots(1, 2, figsize=(20, 10))
 fig1.suptitle(filename, fontsize=18)
 
+#Grafica de la señal temporal y del espectro en frecuencias
 # Se grafica la señal temporal
 ax1[0].plot(t, senial)
 ax1[0].set_xlabel('Tiempo [s]', fontsize=15)
@@ -54,7 +55,6 @@ ax1[0].grid()
 
 # se grafica la magnitud de la respuesta en frecuencia
 ax1[1].plot(freq, senial_fft_mod)
-#ax1[1].plot(f, senial_fft_mod)
 ax1[1].set_xlabel('Frecuencia [Hz]', fontsize=15)
 ax1[1].set_ylabel('Magnitud [V]', fontsize=15)
 ax1[1].set_title('Magnitud de la Respuesta en Frecuencia', fontsize=15)
@@ -82,10 +82,10 @@ print(sos_iir_3)
 #FIR
 # Proponemos un orden
 N = 1001
-num_fir_3 = signal.firwin(N, cutoff=500, window='hamming', pass_zero='lowpass', fs=fs)
+num_fir_3 = signal.firwin(N, cutoff=1000, window='hamming', pass_zero='lowpass', fs=fs)
 
-print("Coeficientes del filtro:")
-print(num_fir_3)
+#print("Coeficientes del filtro:")
+#print(num_fir_3)
 
 # se genera un vector de frecuencias
 f = np.logspace(-1, 3, int(1e3))
@@ -95,9 +95,7 @@ f, h_fir_3 = signal.freqz(num_fir_3, 1, worN=f, fs=fs)
 
 # se grafican ambas respuestas en frecuencia superpuestas
 fig1, ax1 = plt.subplots(1, 1, figsize=(10, 7), sharex=True)
-#ax1.plot(f, 20*np.log10(abs(h_iir_3)), label='Filtro IIR')
 ax1.plot(f, 20*np.log10(abs(h_fir_3)), label='Filtro FIR')
-#ax1.plot([1, 50], [-20, -20], 'X', label='Requisitos de atenuación')
 ax1.set_ylabel('Ganancia [dB]', fontsize=15)
 ax1.set_xlabel('Frecuencia [Hz]', fontsize=15)
 ax1.grid(which='both')
@@ -106,13 +104,17 @@ ax1.set_title('Filtro IIR vs FIR', fontsize=15)
 ax1.set_xscale('log')
 ax1.set_xlim([1, 10000])
 ax1.set_ylim([-80, 10])
-
 plt.show()
 
 
+
+#Aplico señal al filtro propuesto y se obtiene la señal filtrada
+
 senial_fir = signal.lfilter(num_fir_3, 1, senial)
 
-# graficación de las señales
+
+
+# graficación de las señales Original y filtrada 
 fig1, ax1 = plt.subplots(1, 1, figsize=(15, 10), sharex=True)
 
 
@@ -122,12 +124,49 @@ ax1.set_ylabel('Amplitud [mV]', fontsize=12)
 ax1.set_xlabel('Tiempo [s]', fontsize=12)
 ax1.legend(loc="upper right", fontsize=12)
 ax1.set_title('Filtro FIR', fontsize=15)
-ax1.set_xlim([0, 3])
+ax1.set_xlim([0, 10])
 ax1.set_ylim([-1, 1.5])
 ax1.grid()
 
 plt.show()
 
+
+ts = 1 / fs                             # tiempo de muestreo
+N = len(senial_fir)   
+
+#Grafica del Espectro 
+freq = fft.fftfreq(N, d=1/fs)   # se genera el vector de frecuencias
+senial_fft = fft.fft(senial_fir)    # se calcula la transformada rápida de Fourier
+
+# El espectro es simétrico, nos quedamos solo con el semieje positivo
+f = freq[np.where(freq >= 0)]
+#senial_fft = senial_fft[np.where(freq >= 0)]
+
+# Se calcula la magnitud del espectro
+senial_fft_mod = np.abs(senial_fft) / N     
+
+
+fig1, ax1 = plt.subplots(1, 2, figsize=(20, 10))
+fig1.suptitle(filename, fontsize=18)
+
+#Grafica de la señal temporal y del espectro en frecuencias
+# Se grafica la señal temporal
+ax1[0].plot(t, senial_fir)
+ax1[0].set_xlabel('Tiempo [s]', fontsize=15)
+ax1[0].set_ylabel('Tensión [V]', fontsize=15)
+ax1[0].set_title('Señal temporal', fontsize=15)
+ax1[0].set_xlim([0, ts*N])
+ax1[0].grid()
+
+# se grafica la magnitud de la respuesta en frecuencia
+ax1[1].plot(freq, senial_fft_mod)
+ax1[1].set_xlabel('Frecuencia [Hz]', fontsize=15)
+ax1[1].set_ylabel('Magnitud [V]', fontsize=15)
+ax1[1].set_title('Magnitud de la Respuesta en Frecuencia', fontsize=15)
+ax1[1].set_xlim([-5000, 5000])
+ax1[1].grid()
+
+plt.show()
 
 
 
